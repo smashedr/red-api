7059b60b93cb7b093f2d7b58661c21f33b28c760
Shane <shane@shanerice.me>
2023-05-18 12:37:57 -0700

    Initial COmmit - WIP

--- app/home/views.py
+++ app/home/views.py
@@ -1,11 +1,10 @@
 import json
 import logging
 import httpx
-import time
 from django.conf import settings
 from django.core.cache import cache
-from django.http import HttpRequest, JsonResponse, Http404
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.crypto import get_random_string
 from django_redis import get_redis_connection
@@ -33,11 +32,18 @@ def verify_view(request):
 
         br = cache.get(f'{guild_id}')
         if not br:
-            logger.debug('no cache for: %s', guild_id)
-            br = bot_request(guild_id, ['members', 'guild'])
-            cache.set(f'{guild_id}', br, 300)
+            logger.debug('NO cache for: %s', guild_id)
+            # br = bot_request(guild_id, ['members', 'guild'])
+            br = bot_request('red.pubsub', guild_id, ['members', 'guild'])
+            if br:
+                cache.set(f'{guild_id}', br, 30)
+            else:
+                logger.warning('NO BOT RESPONSE')
+        else:
+            logger.debug('cache call for: %s', guild_id)
+
         data = json.loads(br['data']) if br else None
-        # logger.debug('data: %s', data)
+        logger.debug('data: %s', data)
         if not data:
             context = {'error': 'Error fetching data from Discord.'}
             return render(request, 'verify.html', context)
@@ -69,7 +75,7 @@ def verify_view(request):
         user = request.POST['user']
         logger.debug('user: %s', user)
         data = {'guild': int(guild), 'user': int(user)}
-        br = bot_request(guild, ['verify'], data)
+        br = bot_request('red.captcha', guild, ['verify'], data)
         logger.debug('br: %s', br)
 
         return JsonResponse({}, status=204)
@@ -153,34 +159,88 @@ def send_discord_message(message: str) -> httpx.Response:
     return r
 
 
-def bot_request(guild_id: int | str, requests: list, data: dict = None) -> dict:
+# def bot_request(guild_id: int | str, requests: list, data: dict = None) -> dict:
+#     try:
+#         return_channel = get_random_string(length=16)
+#         r = get_redis_connection('default')
+#         p = r.pubsub(ignore_subscribe_messages=True)
+#         p.subscribe(return_channel)
+#         query = {
+#             'channel': return_channel,
+#             'guild': int(guild_id),
+#             'requests': requests,
+#         }
+#         if data:
+#             query['data'] = data
+#         pub_ret = r.publish('red.pubsub', json.dumps(query))
+#         logger.debug('pub_ret: %s', pub_ret)
+#         if pub_ret == 0:
+#             logger.warning('BOT NOT Listening on pubsub channel: loop')
+#             return dict()
+#         message = None
+#         now = time.time()
+#         timeout = now + 6
+#         while now < timeout:
+#             message = p.get_message(timeout=1)
+#             if message is not None:
+#                 break
+#             time.sleep(0.01)
+#             now = time.time()
+#         return message
+#     except Exception as error:
+#         logger.warning('Error Getting BOT Data')
+#         logger.warning(error)
+#         return dict()
+
+
+def bot_request(channel: int | str,
+                    guild_id: int | str,
+                    requests: list = None,
+                    data: dict = None) -> dict:
     try:
+        # Send Data
         return_channel = get_random_string(length=16)
+        logger.debug('return_channel: %s', return_channel)
         r = get_redis_connection('default')
         p = r.pubsub(ignore_subscribe_messages=True)
         p.subscribe(return_channel)
-        query = {
+        default = {
             'channel': return_channel,
             'guild': int(guild_id),
             'requests': requests,
         }
         if data:
-            query['data'] = data
-        pub_ret = r.publish('red.pubsub', json.dumps(query))
+            logger.debug('data: %s', data)
+            default.update(data)
+        logger.debug('default: %s', default)
+        logger.debug('channel: %s', channel)
+        pub_ret = r.publish(channel, json.dumps(default))
         logger.debug('pub_ret: %s', pub_ret)
         if pub_ret == 0:
             logger.warning('BOT NOT Listening on pubsub channel: loop')
             return dict()
-        message = None
-        now = time.time()
-        timeout = now + 6
-        while now < timeout:
-            message = p.get_message(timeout=1)
-            if message is not None:
-                break
-            time.sleep(0.01)
-            now = time.time()
-        return message
+
+        # Get Data
+
+        # message = p.get_message(timeout=5)
+        # return message
+
+        for message in p.listen():
+            return message
+
+        # message = None
+        # now = time.time()
+        # timeout = now + 6
+        # while now < timeout:
+        #     message = p.get_message(timeout=None)
+        #     logger.debug('-'*20)
+        #     logger.debug(message)
+        #     if message is not None:
+        #         break
+        #     time.sleep(0.01)
+        #     now = time.time()
+        # return message
+
     except Exception as error:
         logger.warning('Error Getting BOT Data')
         logger.warning(error)
